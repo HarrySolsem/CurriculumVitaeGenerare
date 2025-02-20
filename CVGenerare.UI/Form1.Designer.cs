5b69d3ac8d8b3a46175ce5a790fa0ba73a7386dd
Harry Solsem <harry@solsem.no>
2025-02-20 23:20:44 +0100

    init versjon av UI

--- CVGenerare.UI/Form1.Designer.cs
+++ CVGenerare.UI/Form1.Designer.cs
@@ -28,12 +28,204 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            groupBox1 = new GroupBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
+            checkBox5 = new CheckBox();
+            checkBox6 = new CheckBox();
+            checkBox7 = new CheckBox();
+            checkBox8 = new CheckBox();
+            checkBox9 = new CheckBox();
+            checkBox10 = new CheckBox();
+            checkBox11 = new CheckBox();
+            checkBox12 = new CheckBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button1.Location = new Point(310, 415);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Generer CV";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(checkBox7);
+            groupBox1.Controls.Add(checkBox8);
+            groupBox1.Controls.Add(checkBox9);
+            groupBox1.Controls.Add(checkBox10);
+            groupBox1.Controls.Add(checkBox11);
+            groupBox1.Controls.Add(checkBox12);
+            groupBox1.Controls.Add(checkBox4);
+            groupBox1.Controls.Add(checkBox5);
+            groupBox1.Controls.Add(checkBox6);
+            groupBox1.Controls.Add(checkBox3);
+            groupBox1.Controls.Add(checkBox2);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Location = new Point(24, 25);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(241, 360);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Elementer";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(21, 45);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(142, 19);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Personlig informasjon";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(21, 70);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(85, 19);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "Utdannelse";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(21, 95);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(67, 19);
+            checkBox3.TabIndex = 2;
+            checkBox3.Text = "Erfaring";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(21, 170);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(98, 19);
+            checkBox4.TabIndex = 5;
+            checkBox4.Text = "Sertifiseringer";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Location = new Point(21, 145);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new Size(52, 19);
+            checkBox5.TabIndex = 4;
+            checkBox5.Text = "Skills";
+            checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            checkBox6.AutoSize = true;
+            checkBox6.Location = new Point(21, 120);
+            checkBox6.Name = "checkBox6";
+            checkBox6.Size = new Size(78, 19);
+            checkBox6.TabIndex = 3;
+            checkBox6.Text = "Prosjekter";
+            checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            checkBox7.AutoSize = true;
+            checkBox7.Location = new Point(21, 320);
+            checkBox7.Name = "checkBox7";
+            checkBox7.Size = new Size(81, 19);
+            checkBox7.TabIndex = 11;
+            checkBox7.Text = "Frivillighet";
+            checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            checkBox8.AutoSize = true;
+            checkBox8.Location = new Point(21, 295);
+            checkBox8.Name = "checkBox8";
+            checkBox8.Size = new Size(143, 19);
+            checkBox8.TabIndex = 10;
+            checkBox8.Text = "Annen arbeidserfaring";
+            checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            checkBox9.AutoSize = true;
+            checkBox9.Location = new Point(21, 270);
+            checkBox9.Name = "checkBox9";
+            checkBox9.Size = new Size(184, 19);
+            checkBox9.TabIndex = 9;
+            checkBox9.Text = "Tilbakemelding medarbeidere";
+            checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            checkBox10.AutoSize = true;
+            checkBox10.Location = new Point(21, 245);
+            checkBox10.Name = "checkBox10";
+            checkBox10.Size = new Size(143, 19);
+            checkBox10.TabIndex = 8;
+            checkBox10.Text = "Tilbakemelding ledere";
+            checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // checkBox11
+            // 
+            checkBox11.AutoSize = true;
+            checkBox11.Location = new Point(21, 220);
+            checkBox11.Name = "checkBox11";
+            checkBox11.Size = new Size(159, 19);
+            checkBox11.TabIndex = 7;
+            checkBox11.Text = "Nøkkelroller og resultater";
+            checkBox11.UseVisualStyleBackColor = true;
+            // 
+            // checkBox12
+            // 
+            checkBox12.AutoSize = true;
+            checkBox12.Location = new Point(21, 195);
+            checkBox12.Name = "checkBox12";
+            checkBox12.Size = new Size(162, 19);
+            checkBox12.TabIndex = 6;
+            checkBox12.Text = "Profesjonelt sammendrag";
+            checkBox12.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(397, 450);
+            Controls.Add(groupBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "CV Generare";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private GroupBox groupBox1;
+        private CheckBox checkBox4;
+        private CheckBox checkBox5;
+        private CheckBox checkBox6;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private CheckBox checkBox7;
+        private CheckBox checkBox8;
+        private CheckBox checkBox9;
+        private CheckBox checkBox10;
+        private CheckBox checkBox11;
+        private CheckBox checkBox12;
     }
 }
--- CVGenerare.UI/Form1.cs
+++ CVGenerare.UI/Form1.cs
@@ -6,5 +6,10 @@ namespace CVGenerare.UI
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
